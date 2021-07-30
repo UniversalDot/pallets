759e23f27deabf8902f9fadae0c057ab51043bd6
JosephKnecht <authorJosephKnecht@gmail.com>
2021-07-30 16:15:06 +0200

    add tests

--- profile/src/tests.rs
+++ profile/src/tests.rs
@@ -1,59 +1,7 @@
-use crate::{self as map_set, *};
-use frame_support::{assert_noop, assert_ok, construct_runtime, parameter_types};
-use sp_core::H256;
+use crate::{ *,};
+use frame_support::{assert_noop, assert_ok };
 use sp_io::TestExternalities;
-use sp_runtime::{
-	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
-};
-
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
-type Block = frame_system::mocking::MockBlock<TestRuntime>;
-
-construct_runtime!(
-	pub enum TestRuntime where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Module, Call, Config, Storage, Event<T>},
-		MapSet: map_set::{Module, Call, Storage, Event<T>},
-	}
-);
-
-parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub BlockWeights: frame_system::limits::BlockWeights =
-		frame_system::limits::BlockWeights::simple_max(1024);
-}
-impl frame_system::Config for TestRuntime {
-	type BaseCallFilter = ();
-	type BlockWeights = ();
-	type BlockLength = ();
-	type Origin = Origin;
-	type Index = u64;
-	type Call = Call;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = BlockHashCount;
-	type DbWeight = ();
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ();
-}
-
-impl Config for TestRuntime {
-	type Event = Event;
-}
+use crate::{mock::{Event, MapSet, Origin, System, TestRuntime}};
 
 struct ExternalityBuilder;
 
