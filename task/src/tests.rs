55f26e4ff0af44b11633fd1b0d4e6defd57083b0
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-11-24 12:20:11 +0100

    add additional test for task count

--- task/src/tests.rs
+++ task/src/tests.rs
@@ -43,3 +43,22 @@ fn increase_task_count_when_creating_task(){
 		assert_eq!(Task::task_count(), 1);
 	});
 }
+
+#[test]
+fn increase_task_count_when_creating_two_tasks(){
+	new_test_ext().execute_with( || {
+
+		let mut vec1 = Vec::new();
+		vec1.push(2);
+
+		let mut vec2 = Vec::new();
+		vec2.push(7);
+		
+		// Ensure new task can be creted with [signer, requirements vector, buget]
+		assert_ok!(Task::create_task(Origin::signed(1), vec1, 7));
+		assert_ok!(Task::create_task(Origin::signed(1), vec2, 99));
+
+		// Assert that count is incremented to 2 after task creation
+		assert_eq!(Task::task_count(), 2);
+	});
+}
