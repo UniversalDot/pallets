be2a3e50ce29018efb35dc7ea89829fedf2d4384
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-12-08 10:55:09 +0100

    add create org function

--- dao/src/lib.rs
+++ dao/src/lib.rs
@@ -75,7 +75,7 @@ pub mod pallet {
 	#[pallet::storage]
 	#[pallet::getter(fn vision_signers)]
 	// Store DaoSigners in StorageMap as Vector with value: [AccountID, Hash of Vision]
-	pub(super) type DaoMembers<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, Vec<T::Hash>, ValueQuery>;
+	pub(super) type Organizations<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, Vec<T::Hash>, ValueQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events
@@ -151,6 +151,7 @@ pub mod pallet {
             Ok(())
         }
 
+		// TODO implement logic
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
@@ -172,6 +173,24 @@ pub mod pallet {
 			Ok(())
 		}
 
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn create_organization(origin: OriginFor<T>, org_name: Vec<u8>, vision_document: Vec<u8>) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let who = ensure_signed(origin)?;
+
+			let hash_vision = T::Hashing::hash_of(&vision_document);
+
+
+			// Update storage.
+			// <DaoMembers<T>>::insert(who, hash_vision);
+
+			// Emit an event.
+			Self::deposit_event(Event::VisionSigned(who, vision_document));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
 		
 	}
 }
