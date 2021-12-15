7263f8fada247e683c7f2de25c9212239b1e3b33
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-12-15 14:43:38 +0100

    create profile struct function

--- profile/src/benchmarking.rs
+++ profile/src/benchmarking.rs
@@ -7,6 +7,32 @@ use crate::Pallet as Template;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 
+use frame_support::{
+	traits::{Currency}};
+
+
+
+// This creates an `Profile` object.
+// All data is pre-populated with some arbitrary bytes.
+fn create_profile_info<T: Config>(num_fields: u32) -> Profile<T> {
+	// let data = Data::Raw(vec![0; 32].try_into().unwrap());
+	let mut interests = Vec::new();
+	interests.push(77);
+
+	let caller: T::AccountId = whitelisted_caller();
+
+	let balance = T::Currency::free_balance(&caller);
+
+	let info = Profile {
+		owner: caller,
+		interests: interests,
+		balance: Some(balance),
+		reputation: 1111111,
+	};
+
+	return info
+}
+
 
 benchmarks! {
 	benchmark_name {
