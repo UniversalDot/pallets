f0248cf322de99dd073e88a55087a16930bf6847
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-12-16 11:30:40 +0100

    add benchmarking template

--- task/src/benchmarking.rs
+++ task/src/benchmarking.rs
@@ -8,13 +8,14 @@ use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_call
 use frame_system::RawOrigin;
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
-	}
+	benchmark_name {
+		/* setup initial state */
+	  }: {
+		/* the code to be benchmarked */
+	  }
+	  verify {
+		/* verifying final state */
+	  }
 }
 
 impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test,);
