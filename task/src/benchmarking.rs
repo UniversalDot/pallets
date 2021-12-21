dc06ec7ca9da404410d544c8d820109e75698678
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-12-21 16:06:14 +0100

    add create task benchmark

--- task/src/benchmarking.rs
+++ task/src/benchmarking.rs
@@ -6,7 +6,10 @@ use super::*;
 use crate::Pallet as PalletTask;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
-use frame_support::traits::Currency;
+use frame_support::traits::{Currency};
+use frame_support::sp_runtime::traits::Hash;
+use std::convert::TryInto;
+use sp_std::ptr::hash;
 
 // Helper function to assert event thrown during verification
 fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
@@ -24,7 +27,7 @@ fn create_task_info<T: Config>(_num_fields: u32) -> Task<T> {
 	let volunteer: T::AccountId = whitelisted_caller();
 	let owner: T::AccountId = whitelisted_caller();
 	let balance = T::Currency::total_balance(&initiator);
-	let deadline = u64::MAX;
+	let deadline = u32::MAX;
 	let status: TaskStatus = TaskStatus::InProgress;
 
 	// Create object
@@ -54,11 +57,27 @@ benchmarks! {
 
 	create_task {
 		/* setup initial state */
-	}: {
-		/* the code to be benchmarked */
-	}
+		let caller: T::AccountId = whitelisted_caller();
+
+		let task = create_task_info::<T>(1);
+
+		let s in 1 .. u8::MAX.into(); // max bytes for requirements
+		let x in 1 .. 2000; 
+
+		let requirements = vec![0u8, s as u8];
+		let budget = T::Currency::total_balance(&caller);
+
+		let task_hash = PalletTask::<T>::new_task(&caller, &requirements, &budget, &x)?;
+
+	}: 
+	/* the code to be benchmarked */
+	create_task(RawOrigin::Signed(caller), requirements, budget, x.into())
+	
 	verify {
 		/* verifying final state */
+		let caller: T::AccountId = whitelisted_caller();
+		// TODO: fix task hash error
+		assert_last_event::<T>(Event::<T>::TaskCreated(caller, task_hash).into());
 	}
 }
 
