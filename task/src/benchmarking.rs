ca4adc1ad36417a538fc91117c30c51d0f54fae1
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-12-21 11:50:26 +0100

    update name

--- task/src/benchmarking.rs
+++ task/src/benchmarking.rs
@@ -1,9 +1,9 @@
-//! Benchmarking setup for pallet-template
+//! Benchmarking setup for pallet-task
 
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as PalletTask;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 
@@ -18,4 +18,4 @@ benchmarks! {
 	  }
 }
 
-impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test,);
+impl_benchmark_test_suite!(PalletTask, crate::mock::new_test_ext(), crate::mock::Test,);
