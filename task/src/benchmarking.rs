73286270e2d366132046e9257bf42bc75137e900
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-12-21 11:51:08 +0100

    add helper

--- task/src/benchmarking.rs
+++ task/src/benchmarking.rs
@@ -7,6 +7,11 @@ use crate::Pallet as PalletTask;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 
+// Helper function to assert event thrown during verification
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
+}
+
 benchmarks! {
 	benchmark_name {
 		/* setup initial state */
