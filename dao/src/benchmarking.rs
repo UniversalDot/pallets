5ffd77c863ad4e56402142ca520e4ecf8f6f403e
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2022-01-04 11:44:57 +0100

    add members benchmark completed

--- dao/src/benchmarking.rs
+++ dao/src/benchmarking.rs
@@ -21,8 +21,10 @@ use super::*;
 
 #[allow(unused)]
 use crate::Pallet as PalletDao;
-use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
+
+const SEED: u32 = 0;
 
 // Helper function to assert event thrown during verification
 fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
@@ -118,22 +120,26 @@ benchmarks! {
 		assert_last_event::<T>(Event::<T>::OrganizationCreated( caller, name).into())
 	}
 
-	//   add_members {
-	// 	/* setup initial state */
-	// 	let caller: T::AccountId = whitelisted_caller();
+	add_members {
+		/* setup initial state */
+		let caller: T::AccountId = whitelisted_caller();
 
-	// 	let s in 1 .. u8::MAX.into();
-	// 	let name = vec![0u8, s as u8];
+		let s in 1 .. u8::MAX.into();
+		let name = vec![0u8, s as u8];
 		
-	// 	let account: T::AccountId = whitelisted_caller();
+		// Create account for member
+		let account: T::AccountId = account("member", s, SEED);
+
+		// Create organization before adding members to it
+		PalletDao::<T>::create_organization(RawOrigin::Signed(caller.clone()).into(), name.clone());
 
 
-	//   }: add_members(RawOrigin::Signed(caller.clone()), name.clone(), account.clone())
-	// 	/* the code to be benchmarked */
-	//   verify {
-	// 	/* verifying final state */
-	// 	assert_last_event::<T>(Event::<T>::MemberAdded (caller, account ).into());
-	//   }
+	}: add_members(RawOrigin::Signed(caller.clone()), name.clone(), account.clone())
+		/* the code to be benchmarked */
+	verify {
+		/* verifying final state */
+		assert_last_event::<T>(Event::<T>::MemberAdded (caller, account ).into());
+	}
 }
 
 impl_benchmark_test_suite!(PalletDao, crate::mock::new_test_ext(), crate::mock::Test,);
